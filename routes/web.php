dbb650aea1fb0f622f70b26c54b162124c3a6b95
Vancrew <im26vancrew@gmail.com>
2017-03-15 14:15:26 +0700

    templating admin lte

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin', function () {
+    return view('template.main');
+
+});
+
+
+Route::get('home', 'HomeController@index');
